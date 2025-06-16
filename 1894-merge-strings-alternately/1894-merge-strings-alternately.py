904d56960e23019f55493b6dc1b5290bad95a06c
kshitijkale <50714792+kshitijkale@users.noreply.github.com>
2025-06-16 19:41:58 +0530

    Time: 39 ms (49.16%), Space: 17.6 MB (73.82%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -1,23 +1,23 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        n1 = len(word1)
-        n2 = len(word2)
-        count1 = 0
-        count2 = 0
-        sol =""
-        while (count1 !=n1 and count2 != n2):
-            count = count1+count2
+        n1=len(word1)
+        n2=len(word2)
+        word = ""
+        count1=0
+        count2=0
+
+        while count1!= n1 and count2!= n2:
+            count = count1 + count2
             if(count%2==0):
-                sol += word1[count1]
+                word += word1[count1]
                 count1+=1
             else:
-                sol += word2[count2]
+                word += word2[count2]
                 count2+=1
-
-        if(count1 == n1):
-            for x in word2[count2:]:
-                sol += x
-        if(count2 ==  n2):
-            for x in word1[count1:]:
-                sol += x
-        return sol
+        while count1!=n1:
+            word += word1[count1]
+            count1+=1
+        while count2!=n2:
+            word += word2[count2]
+            count2+=1
+        return word
