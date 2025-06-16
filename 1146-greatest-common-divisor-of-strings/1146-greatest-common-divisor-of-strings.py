2ecd58fa440447606419b79c8cbfdb891b05c4a5
kshitijkale <50714792+kshitijkale@users.noreply.github.com>
2025-06-16 19:35:51 +0530

    Time: 0 ms (100%), Space: 17.8 MB (71.03%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -1,13 +1,14 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        len1 = len(str1)
-        len2 = len(str2)
+        len1 =len(str1)
+        len2= len(str2)
         def isdivisor(l):
             if len1%l or len2%l:
                 return False
             can = min(str1,str2)
             m1,m2 = len1//l , len2//l
             return can[:l]*m1 == str1 and can[:l]*m2 == str2
+
         for l in range(min(len1,len2),0,-1):
             if(isdivisor(l)):
                 return str1[:l]
